2197d1ce957b617bab67a904d6c9eddd6601d55a
lechug1122 <101825920+lechug1122@users.noreply.github.com>
2022-03-17 11:59:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cecybook/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cecybook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
